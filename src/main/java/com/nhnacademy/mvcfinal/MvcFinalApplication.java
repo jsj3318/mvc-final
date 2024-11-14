21eab9aaed12b5cbdfb93443ad9754766a49abb2
조승주 <tmdwn3318@gmail.com>
2024-11-14 09:59:21 +0900

    init

---
+++ src/main/java/com/nhnacademy/mvcfinal/MvcFinalApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mvcfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/mvcfinal/MvcFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.mvcfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
